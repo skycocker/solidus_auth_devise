6a63a01a76ca9d92d55048793648bbe973a14193
Michał Siwek <mike21@aol.pl>
2018-10-29 15:46:44 +0100

    Wrap up devise migrations in conditions

--- db/migrate/20101026184950_rename_columns_for_devise.rb
+++ db/migrate/20101026184950_rename_columns_for_devise.rb
@@ -1,24 +1,94 @@
 class RenameColumnsForDevise < SolidusSupport::Migration[4.2]
   def up
-    return if column_exists?(:spree_users, :password_salt)
     if column_exists?(:spree_users, :crypted_password)
       rename_column :spree_users, :crypted_password, :encrypted_password
     end
+
     if column_exists?(:spree_users, :salt)
       rename_column :spree_users, :salt, :password_salt
+    elsif !column_exists?(:spree_users, :password_salt)
+      add_column :spree_users, :password_salt, :string
     end
-    rename_column :spree_users, :remember_token_expires_at, :remember_created_at
-    rename_column :spree_users, :login_count, :sign_in_count
-    rename_column :spree_users, :failed_login_count, :failed_attempts
-    rename_column :spree_users, :single_access_token, :reset_password_token
-    rename_column :spree_users, :current_login_at, :current_sign_in_at
-    rename_column :spree_users, :last_login_at, :last_sign_in_at
-    rename_column :spree_users, :current_login_ip, :current_sign_in_ip
-    rename_column :spree_users, :last_login_ip, :last_sign_in_ip
-    add_column :spree_users, :authentication_token, :string
-    add_column :spree_users, :unlock_token, :string
-    add_column :spree_users, :locked_at, :datetime
-    remove_column :spree_users, :openid_identifier
+
+    if column_exists?(:spree_users, :remember_token_expires_at)
+      if column_exists?(:spree_users, :remember_created_at)
+        remove_column :spree_users, :remember_token_expires_at
+      else
+        rename_column :spree_users, :remember_token_expires_at, :remember_created_at
+      end
+    end
+
+    if column_exists?(:spree_users, :login_count)
+      if column_exists?(:spree_users, :sign_in_count)
+        remove_column :spree_users, :login_count
+      else
+        rename_column :spree_users, :login_count, :sign_in_count
+      end
+    end
+
+    if column_exists?(:spree_users, :failed_login_count)
+      if column_exists?(:spree_users, :failed_attempts)
+        remove_column :spree_users, :failed_login_count
+      else
+        rename_column :spree_users, :failed_login_count, :failed_attempts
+      end
+    end
+
+    if column_exists?(:spree_users, :single_access_token)
+      if column_exists?(:spree_users, :reset_password_token)
+        remove_column :spree_users, :single_access_token
+      else
+        rename_column :spree_users, :single_access_token, :reset_password_token
+      end
+    end
+
+    if column_exists?(:spree_users, :current_login_at)
+      if column_exists?(:spree_users, :current_sign_in_at)
+        remove_column :spree_users, :current_login_at
+      else
+        rename_column :spree_users, :current_login_at, :current_sign_in_at
+      end
+    end
+
+    if column_exists?(:spree_users, :last_login_at)
+      if column_exists?(:spree_users, :last_sign_in_at)
+        remove_column :spree_users, :last_login_at
+      else
+        rename_column :spree_users, :last_login_at, :last_sign_in_at
+      end
+    end
+
+    if column_exists?(:spree_users, :current_login_ip)
+      if column_exists?(:spree_users, :current_sign_in_ip)
+        remove_column :spree_users, :current_login_ip
+      else
+        rename_column :spree_users, :current_login_ip, :current_sign_in_ip
+      end
+    end
+
+    if column_exists?(:spree_users, :last_login_ip)
+      if column_exists?(:spree_users, :last_sign_in_ip)
+        remove_column :spree_users, :last_login_ip
+      else
+        rename_column :spree_users, :last_login_ip, :last_sign_in_ip
+      end
+    end
+
+    unless column_exists?(:spree_users, :authentication_token)
+      add_column :spree_users, :authentication_token, :string
+    end
+
+    unless column_exists?(:spree_users, :unlock_token)
+      add_column :spree_users, :unlock_token, :string
+    end
+
+    unless column_exists?(:spree_users, :locked_at)
+      add_column :spree_users, :locked_at, :datetime
+    end
+
+    if column_exists?(:spree_users, :openid_identifier)
+      remove_column :spree_users, :openid_identifier
+    end
   end
 
   def down
